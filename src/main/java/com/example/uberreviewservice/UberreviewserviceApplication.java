772623570978251341e44b29c5458e48299f896a
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-08-30 15:39:24 +0530

    Initial Commit

---
+++ src/main/java/com/example/uberreviewservice/UberreviewserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberreviewserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberreviewserviceApplication.class, args);
+	}
+
+}
